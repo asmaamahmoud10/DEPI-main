332178d9f188b515526b39abbe5309933b377280
Asmaa Mahmoud <asmaamahmoud17112002@gmail.com>
2025-04-15 16:54:02 +0200

    Add delete,view, edit buttons

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -16,4 +16,28 @@ export class HomeComponent implements OnInit {
       this.notes = data;
     });
   }
+
+  noteToDelete: any = null;
+
+viewNote(note: any) {
+  console.log("Viewing note", note);
+}
+
+editNote(note: any) {
+  console.log("Editing note", note);
+}
+
+confirmDelete(note: any) {
+  this.noteToDelete = note;
+}
+
+cancelDelete() {
+  this.noteToDelete = null;
+}
+
+deleteNote(note: any) {
+  this.notes = this.notes.filter(n => n !== note);
+  this.noteToDelete = null;
+}
+
 }
